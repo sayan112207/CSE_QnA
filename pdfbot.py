cd845ae05e1e34b42027c2488cff95e43291839e
Sayan Banerjee <90127231+sayan112207@users.noreply.github.com>
2024-01-27 21:30:30 +0530

    Update pdfbot.py

--- pdfbot.py
+++ pdfbot.py
@@ -84,7 +84,7 @@ def main():
             encoded_pdf = base64.b64encode(file.read()).decode("utf-8")
         return encoded_pdf
 
-    pdf_docs = ["KIIT_CSE_SHB_Updated-1.pdf"]
+    pdf_docs = ["https://github.com/sayan112207/CSE_QnA/blob/main/KIIT_CSE_SHB_Updated-1.pdf"]
     raw_text = get_pdf_text(pdf_docs)
     text_chunks = get_chunks(raw_text)
     get_vector_store(text_chunks)
